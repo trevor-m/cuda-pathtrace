1a10e22279e232db10be308d4a91636f13905b6b
Trevor Morris <trevorm@sbcglobal.net>
2017-11-22 20:48:20 -0800

    Can train using patches and test on the full image. Preprocessing the features is necessary - currently just dividing by map to normalize

--- denoising/load_data.py
+++ denoising/load_data.py
@@ -1,9 +1,11 @@
 import OpenEXR
 import Imath
+import random
 import numpy as np
 
-def load_exr_data(filename):
-  """Loads a multilayer OpenEXR file and returns a list of all features loaded as numpy arrays"""
+def load_exr_data(filename, preprocess=False, concat=False):
+  """Loads a multilayer OpenEXR file and returns a list of all features loaded as numpy arrays.
+  You can use np.concatenate(x, 2) to create a 3D array of size [width, height, 14]"""
   infile = OpenEXR.InputFile(filename)
   color = get_layer(infile, 'Color')
   normal = get_layer(infile, 'Normal')
@@ -13,10 +15,22 @@ def load_exr_data(filename):
   normal_var = get_layer(infile, 'NormalVar')
   albedo_var = get_layer(infile, 'AlbedoVar')
   depth_var = get_layer(infile, 'DepthVar')
-  # you can also use np.stack to create a 3D array of size [width, height, 14]
-  return [color, normal, albedo, depth, color_var, normal_var, albedo_var, depth_var]
+
+  # preprocess
+  if preprocess:
+    depth /= np.max(depth)
+    depth_var /= np.max(depth_var)
+    albedo_var /= np.max(albedo_var)
+    color_var /= np.max(color_var)
+    normal_var /= np.max(normal_var)
+
+  data = [color, normal, albedo, depth, color_var, normal_var, albedo_var, depth_var]
+  if concat:
+    return np.concatenate(data, axis=2)
+  return data
 
 def get_layer(infile, layer_name):
+  """Returns a np array with all channels of a single layer in the EXR file with the matching layer name"""
   # extract channel names
   channel_names = []
   for layer in infile.header()['channels']:
@@ -42,3 +56,50 @@ def get_layer(infile, layer_name):
   for i, name in enumerate(channel_names):
     data[:, :, i] = np.fromstring(infile.channel(name, pt), dtype=np.float32).reshape(size[1], size[0])
   return data
+
+def save_exr_data(filename, data):
+  """"""
+  pass
+
+def get_patches(filename, gt_filename, patch_size=64, num_patches=400, preprocess=False):
+  data = load_exr_data(filename, preprocess=preprocess, concat=True)
+  gt = load_exr_data(gt_filename, preprocess=preprocess)[0]
+  w = data.shape[0]
+  h = data.shape[1]
+
+  candidate_patches = []
+  candidate_patches_gt = []
+  candidate_patch_scores = []
+  total_score = 0
+  for i in range(num_patches*4):
+    # pick random point to be the top-left corner of patch
+    x = random.randint(0, w-patch_size)
+    y = random.randint(0, h-patch_size)
+    patch = data[x:x+patch_size, y:y+patch_size, :]
+    patch_gt = gt[x:x+patch_size, y:y+patch_size, :]
+    score = get_score(patch)
+
+    candidate_patches.append(patch)
+    candidate_patches_gt.append(patch_gt)
+    candidate_patch_scores.append(score)
+    total_score += score
+
+  patches = []
+  patches_gt = []
+  while len(patches) < num_patches:
+    for i, patch in enumerate(candidate_patches):
+      # probability of picking this patch is score/total_score
+      p_pick = candidate_patch_scores[i] / total_score
+      if random.random() < p_pick:
+        # pick this patch and remove it from pool
+        patches.append(patch)
+        patches_gt.append(candidate_patches_gt[i])
+        del candidate_patch_scores[i]
+        del candidate_patches_gt[i]
+        del candidate_patches[i]
+
+  return np.array(patches), np.array(patches_gt)
+
+def get_score(patch):
+  # score is total color variance + total normal variance
+  return np.sum(patch[:, :, 10]) + np.sum(patch[:, :, 11])
--- denoising/train.py
+++ denoising/train.py
@@ -2,7 +2,11 @@ import tensorflow as tf
 import numpy as np
 import argparse
 import skimage.io
-from load_data import load_exr_data
+import datetime
+import time
+import os
+import random
+from load_data import load_exr_data, get_patches
 
 NUM_FEATURE_CHANNELS = 14
 
@@ -18,48 +22,93 @@ def build_model():
   input_features = tf.placeholder(tf.float32, [None, None, None, NUM_FEATURE_CHANNELS], name='input_features')
   gt_color = tf.placeholder(tf.float32, [None, None, None, 3], name='gt_color')
   
-  x = tf.layers.conv2d(input_features, filters=32, kernel_size=(3,3), padding='same', activation=tf.nn.relu)
-  x = tf.layers.conv2d(x, filters=64, kernel_size=(3,3), padding='same', activation=tf.nn.relu)
-  x = tf.layers.conv2d(x, filters=64, kernel_size=(3,3), padding='same', activation=tf.nn.relu)
-  denoised_color = tf.layers.conv2d(x, filters=3, kernel_size=(3,3), padding='same')
+  with tf.variable_scope("denoiser"):
+    x = tf.layers.conv2d(input_features, filters=64, kernel_size=(5,5), padding='same', activation=tf.nn.relu)
+    x = tf.layers.conv2d(x, filters=64, kernel_size=(5,5), padding='same', activation=tf.nn.relu)
+    x = tf.layers.conv2d(x, filters=64, kernel_size=(5,5), padding='same', activation=tf.nn.relu)
+    x = tf.layers.conv2d(x, filters=64, kernel_size=(5,5), padding='same', activation=tf.nn.relu)
+    x = tf.layers.conv2d(x, filters=64, kernel_size=(5,5), padding='same', activation=tf.nn.relu)
+    x = tf.layers.conv2d(x, filters=64, kernel_size=(5,5), padding='same', activation=tf.nn.relu)
+    x = tf.layers.conv2d(x, filters=64, kernel_size=(5,5), padding='same', activation=tf.nn.relu)
+    denoised_color = tf.layers.conv2d(x, filters=3, kernel_size=(5,5), padding='same')
 
   loss = tf.reduce_sum(tf.abs(denoised_color - gt_color))
   train_step = tf.train.AdamOptimizer(learning_rate=1e-4).minimize(loss)
 
   return denoised_color, loss, train_step
 
-def test_model(iteration, sess, output, loss, data_in, data_gt):
+def test_model(iteration, sess, log_path, output, loss, data_in, data_gt):
   """Compute the loss and save out the output image"""
   test_loss = sess.run(loss, feed_dict={'input_features:0': data_in, 'gt_color:0': data_gt})
+  start = time.time()
   test_output = sess.run(output, feed_dict={'input_features:0': data_in, 'gt_color:0': data_gt})
-  save_image(str(iteration)+"_out.png", test_output)
-  save_image(str(iteration)+"_gt.png", data_gt)
-  return test_loss
+  pred_time = time.time()-start
+  save_image(log_path+'/'+str(iteration)+"_out.png", test_output)
+  save_image(log_path+'/'+str(iteration)+"_gt.png", data_gt)
+  save_image(log_path+'/'+str(iteration)+"_in.png", data_in[:, :, :, 0:3])
+  return test_loss, pred_time
+
+def build_output_dir(name=None):
+  log_path = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+  if name:
+    log_path += '_' + name
+  if not os.path.exists(log_path):
+    os.makedirs(log_path)
+  return log_path
+
+def get_batch(data, gt, batch_size):
+  idx = np.random.choice(len(data), batch_size, replace=False)
+  return np.stack(data[idx], 0), np.stack(gt[idx], 0)
 
 def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--load', type=str, help='File to load saved weights from')
+  parser.add_argument('--batch-size', type=int, default=10, help='Batch size')
+  parser.add_argument('--test-freq', type=int, default=1000, help='Will log training status after this many iterations')
+  parser.add_argument('--name', type=str, help='Name for the output folder (a timestamp will be used if not specified')
+  parser.add_argument('--test-only', action='store_true', help='If set, will only test the model and then exit (no training)')
+  args = parser.parse_args()
+
   # create model
   model_output, model_loss, model_train = build_model()
   
-  # load data
-  data = load_exr_data('../4.exr')
-  train_in = np.concatenate(data, axis=2)
-  train_in = train_in.reshape((1, train_in.shape[0], train_in.shape[1], train_in.shape[2]))
-  train_gt = load_exr_data('../20k.exr')[0]
-  train_gt = train_gt.reshape((1, train_gt.shape[0], train_gt.shape[1], train_gt.shape[2]))
-
+  # load training data
+  train_data, train_gt = get_patches('../4.exr', '../20k.exr', preprocess=True)
+  #print(train_data.shape, train_gt.shape)
+  #for i in range(train_gt.shape[0]):
+  #  save_image('patches/patch'+str(i)+'.png', train_gt[i])
+  #return
+  # load testing data
+  test_in = load_exr_data('../4.exr', preprocess=True, concat=True)
+  test_in = test_in.reshape((1, test_in.shape[0], test_in.shape[1], test_in.shape[2]))
+  # load color layer only for gt
+  test_gt = load_exr_data('../20k.exr', preprocess=True)[0]
+  test_gt = test_gt.reshape((1, test_gt.shape[0], test_gt.shape[1], test_gt.shape[2]))
+  
   # start session
   sess = tf.Session()
   sess.run(tf.local_variables_initializer())
   sess.run(tf.global_variables_initializer())
+  saver = tf.train.Saver(tf.trainable_variables())
+  if args.load:
+    saver.restore(sess, args.load)
+
+  # create log path
+  log_path = build_output_dir(args.name)
+  print('Logging results for this session in folder "%s"' % log_path)
 
   for iteration in range(99999999):
     # test every 1000 iterations
-    if iteration % 100 == 0:
-      loss = test_model(iteration, sess, model_output, model_loss, train_in, train_gt)
-      print('[%d] Loss: %.7f' % (iteration, loss))
-    
+    if iteration % args.test_freq == 0:
+      loss, pred_time = test_model(iteration, sess, log_path, model_output, model_loss, test_in, test_gt)
+      print('[%d] Loss: %.7f - Prediction Time: %.5fs' % (iteration, loss, pred_time))
+      # save weights
+      saver.save(sess, os.path.join(log_path, 'weights'), global_step=iteration, write_meta_graph=False)
+      if args.test_only:
+        return
     # train
-    train_loss = sess.run(model_train, feed_dict={'input_features:0': train_in, 'gt_color:0': train_gt})
+    train_data, train_gt = get_batch(train_data, train_gt, batch_size=args.batch_size)
+    train_loss = sess.run(model_train, feed_dict={'input_features:0': train_data, 'gt_color:0': train_gt})
 
 if __name__ == '__main__':
   main()
