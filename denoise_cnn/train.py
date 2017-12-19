ee8950f0496acada87288413e491727605ee47aa
Trevor Morris <trevorm@sbcglobal.net>
2017-12-19 12:30:51 -0800

    Added ability to name output directory and resume training from checkpoint

--- denoise_cnn/train.py
+++ denoise_cnn/train.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 from math import log10
 import time
 import os
+import argparse
 os.environ['CUDA_VISIBLE_DEVICES'] = '1'
 
 import torch
@@ -46,12 +47,12 @@ def validate():
 
 def test(model, boost_tensor):
     # preprocess
-    #boost_tensor[:, :, :3] = torch.clamp(boost_tensor[:, :, :3], 0, 1)
-    boost_tensor[:, :, 9] /=  torch.max(boost_tensor[:, :, 9])
-    boost_tensor[:, :, 10] /=  torch.max(boost_tensor[:, :, 10])
-    boost_tensor[:, :, 11] /=  torch.max(boost_tensor[:, :, 11])
-    boost_tensor[:, :, 12] /=  torch.max(boost_tensor[:, :, 12])
-    boost_tensor[:, :, 13] /=  torch.max(boost_tensor[:, :, 13])
+    boost_tensor[:, :, :3] /= (0.00316 + boost_tensor[:, :, 6:9])
+    boost_tensor[:, :, 9] /= (0.00316 + torch.max(boost_tensor[:, :, 9]))
+    boost_tensor[:, :, 10] /= (0.00316 + torch.max(boost_tensor[:, :, 10]))
+    boost_tensor[:, :, 11] /= (0.00316 + torch.max(boost_tensor[:, :, 11]))
+    boost_tensor[:, :, 12] /= (0.00316 + torch.max(boost_tensor[:, :, 12]))
+    boost_tensor[:, :, 13] /=  (0.00316 + torch.max(boost_tensor[:, :, 13]))
     # convert from (512, 512, 14) to (1, 14, 512, 512)
     boost_tensor = torch.unsqueeze(boost_tensor, 0)
     boost_tensor = boost_tensor.permute(0, 3, 1, 2)
@@ -60,8 +61,8 @@ def test(model, boost_tensor):
     output = model(boost_tensor)
     # transform back to (512, 512, 3)
     output = output.permute(0, 2, 3, 1)
-    output = output.data
-    return output[0]
+    output = output.data[0]
+    return output
 
 def checkpoint(base_dir):
     model_out_path = "{}/model_epoch.pth".format(base_dir)
@@ -69,27 +70,43 @@ def checkpoint(base_dir):
     print("Checkpoint saved to {}".format(model_out_path))
 
 def load_pretrained():
-    cnn = torch.load("/home/moejoe/cs240a/cuda-pathtrace/denoise_cnn/results/12553296/model_epoch.pth").cuda()
+    print(os.getcwd())
+    cnn = torch.load("denoise_cnn/model_epoch.pth").cuda()
     cnn.eval()
     return cnn
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Train denoising algorithm')
+    parser.add_argument('--name', type=str, help='Name for output directory')
+    parser.add_argument('--resume', type=str, help='Name of output directory')
+    parser.add_argument('--resume-epoch', type=int, help='Epoch # to start at')
+    args = parser.parse_args()
+
     print('===> Loading datasets')
     train_set, test_set = get_dataset()
-    training_data_loader = DataLoader(dataset=train_set, num_workers=1, batch_size=2, shuffle=True)
+    training_data_loader = DataLoader(dataset=train_set, num_workers=1, batch_size=5, shuffle=True)
     testing_data_loader = DataLoader(dataset=test_set, num_workers=1, batch_size=1, shuffle=False)
 
     print('===> Building model')
-    model = DenoiseCNN().cuda()
-    #criterion = nn.L1Loss().cuda()
-    criterion = nn.MSELoss().cuda()
+    if args.resume:
+        model = torch.load(args.resume + "/model_epoch.pth").cuda()
+    else:
+        model = DenoiseCNN().cuda()
+    criterion = nn.L1Loss().cuda()
+    #criterion = nn.MSELoss().cuda()
     optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9, dampening=0, weight_decay=0, nesterov=True)
-    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', factor=0.5, patience=500, verbose=True, threshold=1e-4)
+    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', factor=0.5, patience=5000, verbose=True, threshold=1e-4)
 
-    time_str = str(int(time.time()))[2::]
-    base_dir = "results/" + time_str
+    if args.resume:
+        base_dir = args.resume
+    else:
+        time_str = str(int(time.time()))[2::]
+        base_dir = "results/" + time_str
+        if args.name:
+            base_dir += '_' + args.name
 
-    for epoch in range(1, 400001):
+    start_epoch = args.resume_epoch if args.resume_epoch else 1
+    for epoch in range(start_epoch, 400001):
         input, prediction = train(epoch)
         if epoch % 50 == 0:
             if not os.path.exists(base_dir):
